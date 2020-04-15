d2eadb0702b536f294853179a1cf5dd80fb321c8
Nikita <razinkovnik@gmail.com>
2020-04-15 10:11:26 +0300

    try to play vs chess.com

--- chess_com.py
+++ chess_com.py
@@ -1,8 +1,12 @@
 from notation import Notation
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 from board import Board
 from utils import translate_notation
+
+from networks import SimpleLeaner
+from game_analysis import *
 
 
 class ChessCom:
@@ -11,7 +15,9 @@ class ChessCom:
         self.driver.get("https://www.chess.com/play/computer")
         self.driver.implicitly_wait(1)
         self.board = Board()
-        self.step_num = 0
+        model = SimpleLeaner()
+        model.load()
+        self.game = Game(model)
 
     @staticmethod
     def __init_driver():
@@ -24,11 +30,20 @@ class ChessCom:
         flip_btn.click()
 
     def update_board(self):
-        color = self.step_num % 2
-        self.step_num += 1
-        move = self.driver.find_element_by_id(f"movelist_{self.step_num}").text
-        move = translate_notation(move)
-        self.__go(color, move)
+        self.board = Board()
+        step_num = 0
+        while True:
+            try:
+                color = step_num % 2
+                move = self.driver.find_element_by_id(f"movelist_{step_num + 1}").text
+                if move == "":
+                    break
+                move = translate_notation(move)
+                self.__go(color, move)
+            except NoSuchElementException:
+                break
+            step_num += 1
+        self.board.show()
 
     def __go(self, color, move):
         return self.board.play(Notation(self.board, color, move))
@@ -47,4 +62,19 @@ class ChessCom:
     def move(self, pos_from, pos_to):
         self.__cell(pos_from)
         self.__cell(pos_to)
+        self.update_board()
+
+    def predict_update(self):
+        self.game.board = self.board
+        next_move = self.game.predict()
+        if not next_move:
+            print("game end")
+            return
+        fig, pos_from, pos_to, prob = next_move
+        if fig == "O-O":
+            pos_from, pos_to = "e8", "g8"
+        elif fig == "O-O-O":
+            pos_from, pos_to = "e8", "c8"
+        print(f"{fig} {pos_from}-{pos_to}, prob = {prob}")
+        self.move(pos_from, pos_to)
         self.update_board()
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import time
 
 from chess_com import ChessCom
+from networks import SimpleLeaner
+from game_analysis import *
+
+model = SimpleLeaner()
+model.load()
 
 chess = ChessCom()
 time.sleep(3)
 chess.flip_board()
 chess.update_board()
-chess.move("e7", "e5")
-chess.update_board()
-chess.board.show()
+chess.predict_update()
+time.sleep(3)
